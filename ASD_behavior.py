8f045f7f1fd59b1417f1191333211c7f7f27aeb4
Hongli Wang <summerworm@outlook.com>
2026-04-10 10:23:46 -0700

    Add files via upload

---
+++ ASD_behavior.py
@@ -0,0 +1,15 @@
+class ASD_behavior:
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        pass
+
+    def average_performance(self):
+        # calculate average performance for each session, and plot the learning curve
+        pass
+
+
+
+if __name__ == "__main__":
+    root_dir = r'Y:\HongliWang\Miniscope\ASD'
+    ASD_beh = ASD_behavior(root_dir)
+    ASD_beh.average_performance()
