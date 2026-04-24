af78ba29ad8c925975ce9f6a111f3dc6d25eb9f9
sademuessel <sademuessel@berkeley.edu>
2026-04-24 12:56:32 -0700

    Update ASD_behavior.py
    sum/len method to calculate average performance?

--- ASD_behavior.py
+++ ASD_behavior.py
@@ -7,11 +7,16 @@ class ASD_behavior:
         # calculate average performance for each session, and plot the learning curve
         pass
 
-
+def average_perf(root_directory):
+   sum(reward) / len(100)
+    
 
 if __name__ == "__main__":
-    root_dir = r'C:\Users\hongl\Documents\GitHub\ASD_group_work\Data'
+    root_dir = r'C:\ASD_group_work'
     ASD_beh = ASD_behavior(root_dir)
     ASD_beh.average_performance()
 
-#Practice Comment 
+#reroute the data directory to "ASD_group_work" in my personal downloads? (Line beginning with "root_dir")
+#SUM/LEN the correct method?
+
+
